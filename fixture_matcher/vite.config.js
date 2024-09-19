271afcac88a5b7a58068b44a4ae4d83f1b8ea144
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2024-09-18 23:08:22 -0700

    Set up for github pages

--- fixture_matcher/vite.config.js
+++ fixture_matcher/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import react from "@vitejs/plugin-react";
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -9,7 +10,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    react()
   ],
+  base: './Fixture-Matcher',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
